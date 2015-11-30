05637d521116d3a88137b9101be1a522ffb579c3
Dwayne Slater <ds84182@gmail.com>
2015-11-29 21:13:18 -0500

    Terminal: multitude of changes
    Events can now send a done completer.
    Export the rest of the API.
    Add put and copy events.
    Add the initial term.read implementation.

--- sec/bin/terminal.lua
+++ sec/bin/terminal.lua
@@ -90,11 +90,39 @@ local function startTerminalHandlerThread(state)
 				state.cursorX = state.cursorX+#event.text
 				
 				char = await(state.gpu:invoke("get", state.cursorX, state.cursorY))
+			elseif event.type == "put" then
+				if cursorOn then
+					await(state.gpu:invoke("set", state.cursorX, state.cursorY, char))
+					cursorOn = false
+				end
+				
+				local old = state.cursorBlink
+				state.cursorBlink = false
+				await(state.gpu:invoke("set", event.x, event.y, event.text))
+				state.cursorBlink = old
+				
+				char = await(state.gpu:invoke("get", state.cursorX, state.cursorY))
+			elseif event.type == "copy" then
+				if cursorOn then
+					await(state.gpu:invoke("set", state.cursorX, state.cursorY, char))
+					cursorOn = false
+				end
+				
+				local old = state.cursorBlink
+				state.cursorBlink = false
+				await(state.gpu:invoke("copy", event.x, event.y, event.width, event.height, event.destx-event.x, event.desty-event.y))
+				state.cursorBlink = old
+				
+				char = await(state.gpu:invoke("get", state.cursorX, state.cursorY))
 			end
 		end
 	
 		state.stream:listen(function(event)
 			state.mutex:performAtomic(processEvent, event)
+			
+			if event.done then
+				event.done:complete()
+			end
 		end)
 		
 		while true do
@@ -172,6 +200,67 @@ function term.get(pid, name)
 	return handles[name]
 end
 
+function term.setCursor(pid, handle, x, y)
+	kobject.checkType(handle, "Handle")
+	checkArg(2, x, "number")
+	checkArg(3, y, "number")
+	
+	local state = terminalState[handle:getId()]
+	
+	if state then
+		state.events:send({type = "move_cursor", x = x, y = y})
+	end
+end
+
+function term.getCursor(pid, handle)
+	kobject.checkType(handle, "Handle")
+	
+	local state = terminalState[handle:getId()]
+	
+	if state then
+		local completer, future = kobject.newFuture()
+		state.events:send({type = "sync", done = completer})
+		return future:after(function()
+			return state.cursorX, state.cursorY
+		end)
+	end
+end
+
+function term.setCursorBlink(pid, handle, blink)
+	kobject.checkType(handle, "Handle")
+	checkArg(2, blink, "boolean")
+	
+	local state = terminalState[handle:getId()]
+	
+	if state then
+		state.events:send({type = "set_blink", blink = blink})
+	end
+end
+
+function term.getCursorBlink(pid, handle)
+	kobject.checkType(handle, "Handle")
+	
+	local state = terminalState[handle:getId()]
+	
+	if state then
+		local completer, future = kobject.newFuture()
+		state.events:send({type = "sync", done = completer})
+		return future:after(function()
+			return state.cursorBlink
+		end)
+	end
+end
+
+function term.getSize(pid, handle)
+	kobject.checkType(handle, "Handle")
+	
+	local state = terminalState[handle:getId()]
+	
+	if state then
+		return state.gpu:invoke("getResolution")
+	end
+end
+
 function term.write(pid, handle, str)
 	kobject.checkType(handle, "Handle")
 	str = tostring(str)
@@ -183,6 +272,177 @@ function term.write(pid, handle, str)
 	end
 end
 
+function term.put(pid, handle, x, y, str)
+	kobject.checkType(handle, "Handle")
+	str = tostring(str)
+	
+	local state = terminalState[handle:getId()]
+	
+	if state then
+		state.events:send({type = "put", x = x, y = y, text = str})
+	end
+end
+
+function term.copy(pid, handle, x, y, width, height, destx, desty)
+	kobject.checkType(handle, "Handle")
+	
+	local state = terminalState[handle:getId()]
+	
+	if state then
+		state.events:send({type = "copy", x = x, y = y, width = width, height = height, destx = destx, desty = desty})
+	end
+end
+
+function term.read(pid, handle, signalStream, history, tabResolver, pwchar)
+	kobject.checkType(handle, "Handle")
+	kobject.checkType(signalStream, "ReadStream")
+	checkArg(2, history, "table", "nil")
+	if tabResolver then kobject.checkType(tabResolver, "Export") end
+	checkArg(4, pwchar, "string", "nil")
+	
+	
+	local state = terminalState[handle:getId()]
+	
+	if not state then
+		return nil
+	end
+	
+	local line = "so, this is kinda long. really, this is long. i'm telling you, this is... long.! if you don't... beleive me..."
+	local cursor = #line
+	local baseCursorX, baseCursorY = await(term.getCursor(pid, handle))
+	local width, height = await(term.getSize(pid, handle))
+	local lineWidth = width-baseCursorX+1
+	local scroll = #line-lineWidth+2
+	
+	local function redraw()
+		term.put(pid, handle, baseCursorX, baseCursorY, (" "):rep(lineWidth)) --writes, but does not do wrapping or cursor setting
+		
+		term.put(pid, handle, baseCursorX, baseCursorY, line:sub(scroll+1, scroll+lineWidth+1))
+		
+		term.setCursor(pid, handle, baseCursorX+(cursor-scroll), baseCursorY)
+	end
+	
+	local function checkCursor()
+		local screenSpaceCursor = (cursor-scroll+1)-lineWidth
+		
+		if screenSpaceCursor > 0 then
+			scroll = scroll+screenSpaceCursor
+			
+			--[[term.copy(pid, handle,
+				baseCursorX+screenSpaceCursor, baseCursorY,
+				lineWidth-screenSpaceCursor, 1,
+				baseCursorX, baseCursorY)
+			term.put(pid, handle,
+				baseCursorX+(lineWidth-screenSpaceCursor-1), baseCursorY,
+				line:sub(scroll+(lineWidth-screenSpaceCursor), scroll+lineWidth))
+			
+			if #line-scroll < lineWidth then
+				local c = lineWidth-(#line-scroll)
+				term.put(pid, handle,
+					baseCursorX+lineWidth-c, baseCursorY,
+					(" "):rep(c))
+			end]]
+		elseif screenSpaceCursor <= (-lineWidth)+1 then
+			screenSpaceCursor = screenSpaceCursor+lineWidth
+			scroll = math.max(scroll-(lineWidth+1), 0) --lineWidth+1
+			
+			--[[term.copy(pid, handle,
+				baseCursorX, baseCursorY,
+				lineWidth-screenSpaceCursor, 1,
+				baseCursorX+screenSpaceCursor, baseCursorY)
+			term.put(pid, handle,
+				baseCursorX, baseCursorY,
+				line:sub(scroll+1, scroll+(lineWidth-screenSpaceCursor)+1))
+			
+			if #line-scroll < lineWidth then
+				local c = lineWidth-(#line-scroll)
+				term.put(pid, handle,
+					baseCursorX+lineWidth-c, baseCursorY,
+					(" "):rep(c))
+			end]]
+		end
+		
+		term.setCursor(pid, handle, baseCursorX+(cursor-scroll), baseCursorY)
+		
+		return screenSpaceCursor > 0
+	end
+	
+	local function insert(str)
+		if cursor == #line then
+			line = line..str
+		elseif cursor == 0 then
+			line = str..line
+		else
+			line = line:sub(1, cursor-1)..str..line:sub(cursor+1)
+		end
+		
+		cursor = cursor+#str
+		
+		if not checkCursor() then
+			--term.put(pid, handle, baseCursorX+(cursor-scroll-1), baseCursorY, str)
+		end
+		
+		redraw()
+	end
+	
+	local function backspace()
+		if cursor == #line+1 then
+			line = line:sub(1, -2)
+		elseif cursor > 0 then
+			line = line:sub(1, cursor-1)..line:sub(cursor+1)
+		else
+			return
+		end
+		
+		cursor = cursor-1
+		
+		if not checkCursor() then
+			--[[local copyWidth = math.min(#line-cursor, lineWidth-(cursor-scroll))
+			term.copy(pid, handle,
+				baseCursorX+(cursor-scroll)+1, baseCursorY,
+				copyWidth, 1,
+				baseCursorX+(cursor-scroll), baseCursorY)
+			
+			local rep = line:sub(cursor+copyWidth, cursor+copyWidth)
+			if #rep == 0 then rep = " " end
+			os.logf("TERM", "copychar %s", rep)
+			term.put(pid, handle,
+				baseCursorX+(cursor-scroll)+copyWidth, baseCursorY,
+				rep)]]
+			--redraw() --TODO: Proper drawing logic
+		else
+			--term.put(pid, handle, baseCursorX+(cursor-scroll), baseCursorY, " ")
+			--redraw()
+		end
+		
+		os.logf("TERM", "%d %d %s", cursor, scroll, line)
+		redraw()
+	end
+	
+	redraw()
+	
+	signalStream:listen(function(signal)
+		local typ = table.remove(signal, 1)
+		
+		if typ == "key_down" then
+			local char = signal[2]
+			if not (char < 0x20 or (char >= 0x7F and char <= 0x9F)) then
+				insert(string.char(signal[2]))
+			elseif signal[3] == 0x0E then
+				backspace()
+			elseif signal[3] == 0xCB and cursor > 0 then
+				cursor = cursor-1
+				checkCursor()
+				redraw()
+			elseif signal[3] == 0xCD and cursor < #line then
+				cursor = cursor+1
+				checkCursor()
+				redraw()
+			end
+		end
+	end)
+end
+
 --Init:
 
 local gpu = component.list("gpu", true)()
@@ -190,6 +450,12 @@ local gpu = component.list("gpu", true)()
 if gpu then
 	local handle = term.add(proc.getCurrentProcess(), "main", gpu)
 	term.write(proc.getCurrentProcess(), handle, "Terminal is working!")
+	term.getCursor(proc.getCurrentProcess(), handle):after(function(x, y)
+		os.logf("TERM", "Tp %d %d", x, y)
+	end)
+	local width = await(term.getSize(proc.getCurrentProcess(), handle))
+	term.setCursor(proc.getCurrentProcess(), handle, 1, 2)--width-5, 2)
+	term.read(proc.getCurrentProcess(), handle, await(getSignalStream()))
 end
 
 --Service Exporting:

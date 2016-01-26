a966bd02eefc4d6df31c1b9f1e441d31e6d211e2
Dwayne Slater <ds84182@gmail.com>
2016-01-26 11:18:11 -0500

    Startup: Interpret options from file

--- sec/bin/startup.lua
+++ sec/bin/startup.lua
@@ -1,11 +1,24 @@
 --Startup: reads /sec/etc/startup for the next executables to run
 
-local function execute(path)
+local function split(line, pattern)
+	local tab = {}
+	
+	for s in line:gmatch("([^"..pattern.."]+)") do
+		tab[#tab+1] = s
+	end
+	
+	return tab
+end
+
+local function execute(path, options)
 	local handle, err = await(fs.open(path, "r"))
 	if handle then
 		local source = await(fs.readAsStream(handle, math.huge):join())
 		fs.close(handle)
-		local pid, err = await(proc.spawn(source, path:match("/?([^/]+)$"):gsub("%.lua$", ""), nil, {}))
+		
+		local trustLevel = nil
+		if options.untrusted then trustLevel = 1000 end
+		local pid, err = await(proc.spawn(source, path:match("/?([^/]+)$"):gsub("%.lua$", ""), nil, nil, trustLevel))
 		
 		if not pid then
 			os.logf("EXECUTE", "Error while starting %s: %s", path, err)
@@ -25,7 +38,24 @@ fs.open("/sec/etc/startup", "r"):after(function(handle)
 			:where(function(line) return line:sub(1,1) ~= "#" and #line > 0 end)
 			:listen(function(line)
 				os.logf("MAIN", "Starting %s", line)
-				execute(line)
+				local s = split(line, " ")
+				local options = {}
+				
+				for i=2, #s do
+					local opt = s[i]
+					
+					local key,val = opt:match("^(.-)=(.+)$")
+					
+					if key then
+						--TODO: Parse val
+						options[key] = val
+					else
+						os.logf("MAIN", "%s", opt)
+						options[opt] = true
+					end
+				end
+				
+				execute(s[1], options)
 			end)
 			:onClose(function()
 				fs.close(handle)

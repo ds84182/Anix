6fbb3bd5d27fff805114d0a24b55c6cf0421c111
Dwayne Slater <ds84182@gmail.com>
2016-10-10 12:06:00 -0400

    Secure Libraries: Update to the new kernel

--- sec/lib/io.lua
+++ sec/lib/io.lua
@@ -1,6 +1,6 @@
 local io = {}
 
-io.debug = false
+io.debug = true
 
 --We use the STDIN, STDOUT, and STDERR env vars
 --They can be Streams or Exports
@@ -13,69 +13,110 @@ local function debug(...)
 	if io.debug then os.logf("IOLIB", ...) end
 end
 
-local stdout, stdin, stdinLF, stderr
+local stdout, stdin, stdinLF, stderr, mutex
+local initing, init = false, false
+local initCompleters = {}
 
 function io.init()
-	debug "Getting IO Streams"
-	
-	stdout = proc.getEnv "STDOUT"
-	
-	if not stdout then
-		debug "Using STDOUT Terminal Implementation"
-		local term = require "term"
-		term.init() --init before setting up streams
-		local rs, ws = kobject.newStream()
-		stdout = ws
-		rs:listen(function(lines)
-			lines = tostring(lines):gsub("\t", "    ")
-			for line in lines:gmatch("[^\n]*\n?") do
-				if line:sub(-1,-1) == "\n" then
-					term.write(line:sub(1,-2))
-					--term.nextLine()
-					local x, y = term.getCursor()
-					term.setCursor(1, y+1)
-				else
-					term.write(line)
-				end
-			end
-		end)
-	end
-	
-	stdin = proc.getEnv "STDIN"
-	
-	if not stdin then
-		--TODO: In order for this to work, term needs to implement something like VT100 (Or something custom :P)
-		debug "Using STDIN Terminal Implementation"
-		local term = require "term"
-		stdin = await(getSignalStream())
-			:where(function(sig)
-				return sig[1] == "key_down" and sig[3] >= 0x20 and (sig[3] < 0x7F or sig[3] > 0x9F)
-			end)
-			:map(function(sig)
-				return string.char(sig[3])
-			end)
-		kobject.setLabel(stdin, "Terminal STDIN")
-		local export, exportClient = kobject.newExport({read = true}, function(method, arguments, pid)
-			return await(term.read())
-		end)
-		kobject.setLabel(export, "Terminal STDIN LF")
-		stdinLF = exportClient
-	else
-		stdinLF = proc.getEnv "STDIN_LF"
-		
-		if not stdinLF and kobject.isA(stdinLF, "ReadStream") then
-			stdinLF = stdin:duplicate():transform(utils.newLineSplitter())
-		end
-	end
-	
-	kobject.declareWeak(stdin)
-	kobject.declareWeak(stdinLF)
+  if init then
+    return kobject.newCompletedFuture()
+  end
+  
+  if not initing then
+    async(function()
+      debug "Creating Kernel Mutex"
+      mutex = kobject.newMutex()
+  
+      debug "Getting IO Streams"
+
+      stdout = proc.getEnv "STDOUT"
+
+      if not stdout then
+        debug "Using STDOUT Terminal Implementation"
+        local term = require "term"
+        term.init() --init before setting up streams
+        local rs, ws = kobject.newStream()
+        stdout = ws
+        rs:listen(function(lines)
+          debug("Enter stdout term %s", lines)
+          lines = tostring(lines):gsub("\t", "    ")
+          mutex:performAtomic(makeAsync(function()
+            debug("Really enter stdout term %s", lines)
+            for line in lines:gmatch("[^\n]*\n?") do
+              if line:sub(-1,-1) == "\n" then
+                term.write(line:sub(1,-2))
+                --term.nextLine()
+                local x, y = term.getCursor()
+                local w, h = term.getSize()
+                local ny = y+1
+                if ny > h then
+                  term.scrollVertical(ny-h)
+                  await(term.setCursor(1, h))
+                else
+                  await(term.setCursor(1, ny))
+                end
+              else
+                await(term.write(line))
+              end
+            end
+            debug("Exit stdout term %s", lines)
+          end))
+        end)
+      end
+
+      stdin = proc.getEnv "STDIN"
+
+      if not stdin then
+        --TODO: In order for this to work, term needs to implement something like VT100 (Or something custom :P)
+        debug "Using STDIN Terminal Implementation"
+        local term = require "term"
+        stdin = await(getSignalStream())
+          :where(function(sig)
+            return sig[1] == "key_down" and sig[3] >= 0x20 and (sig[3] < 0x7F or sig[3] > 0x9F)
+          end)
+          :map(function(sig)
+            return string.char(sig[3])
+          end)
+        kobject.setLabel(stdin, "Terminal STDIN")
+        local export, exportClient = kobject.newExport({read = true}, function(method, arguments, pid)
+          return mutex:performAtomic(makeAsync(function()
+            return await(term.read())
+          end))
+        end)
+        kobject.setLabel(export, "Terminal STDIN LF")
+        stdinLF = exportClient
+      else
+        stdinLF = proc.getEnv "STDIN_LF"
+
+        if not stdinLF and kobject.isA(stdinLF, "ReadStream") then
+          stdinLF = stdin:duplicate():transform(utils.newLineSplitter())
+        end
+      end
+
+      kobject.declareWeak(stdin)
+      kobject.declareWeak(stdinLF)
+    
+      init = true
+    
+      for i, completer in pairs(initCompleters) do
+        completer:complete()
+      end
+      initCompleters = nil
+    end)
+    
+    initing = true
+  end
+  
+  local completer, future = kobject.newFuture()
+  initCompleters[#initCompleters+1] = completer
+  return future
 end
 
-local function read(stream, amount)
+local function read(stream, streamLF, amount)
 	amount = amount or math.huge
 	if kobject.isA(stream, "ExportClient") then
-		return exportClient:invoke("read", amount)
+    local out = stream:invoke("read", amount)
+		return out
 	else
 		--reading bytes--
 		if type(amount) == "number" then
@@ -85,32 +126,33 @@ local function read(stream, amount)
 				return stdin:count(amount):join()
 			end
 		elseif amount == "*l" then
-			error("TODO: Read line")
+			return read(streamLF, nil, 1)
 		end
 	end
 end
 
 function io.write(...)
-	if not stdout then
-		io.init()
-	end
-	
-	stdout:send(table.concat(table.pack(...)))
+  local s = table.concat(table.pack(...))
+	io.init():after(function()
+    stdout:send(s)
+  end)
 end
 
 function io.read(amount)
-	return read(stdin, amount)
+	return io.init():after(function()
+    return read(stdin, stdinLF, amount)
+  end)
 end
 
 function print(...)
 	local tab = table.pack(...)
-	
+
 	if tab.n == 0 then io.write("\n") return end
-	
+
 	for i=1, tab.n do
 		tab[i] = tostring(tab[i])
 	end
-	
+
 	io.write(table.concat(tab, "  ").."\n")
 end
 
--- sec/lib/term.lua
+++ sec/lib/term.lua
@@ -1,6 +1,6 @@
 local term = {}
 
-term.debug = false
+term.debug = true
 
 local ready = false
 local srv = nil
@@ -23,28 +23,28 @@ function term.init()
 	if not waitInit then
 		local completer, future = kobject.newFuture()
 		waitInit = future
-		
+
 		debug("Getting service")
 		srv = await(service.get "TERM")
-	
+
 		if not srv then
 			debug("Service not found!")
 			debug("Waiting for service...")
 			srv = await(await(service.await "TERM"))
 		end
-		
+
 		ready = true
 		debug("Ready!")
-		
+
 		term.switch(proc.getEnv "TERM" or "main")
-		
+
 		completer:complete()
 		waitInit = nil
 	else
 		local oldWaitInit = waitInit
 		local completer, future = kobject.newFuture()
 		waitInit = future
-		
+
 		await(oldWaitInit)
 		completer:complete()
 	end
@@ -56,9 +56,9 @@ function term.get(name)
 	if not ready then
 		term.init()
 	end
-	
+
 	debug("Getting handle for %s", name)
-	
+
 	return await(srv:invoke("get", name))
 end
 
@@ -66,13 +66,13 @@ function term.switch(h)
 	if not ready then
 		term.init()
 	end
-	
+
 	if type(h) == "string" then
 		h = term.get(h)
 	end
-	
+
 	debug("Switching handle %s for %s", handle, h)
-	
+
 	local old = handle
 	handle = h
 	return old
@@ -80,30 +80,31 @@ end
 
 --
 
-function term.sendEvent(event)
-	if not ready then
+function term.sendEventAsync(event)
+  if not ready then
 		term.init()
 	end
-	
-	srv:invoke("sendEvent", handle, event)
+
+	return srv:invoke("sendEvent", handle, event)
+end
+
+function term.sendEvent(event)
+	local completer, future = kobject.newFuture()
+  
+  event.done = completer
+
+  term.sendEventAsync(event)
+	return future
 end
 
 --
 
 function term.getState()
-	local completer, future = kobject.newFuture()
-	
-	term.sendEvent {type = "get_state", done = completer}
-	
-	return future
+	return term.sendEvent {type = "get_state"}
 end
 
 function term.getSize()
-	local completer, future = kobject.newFuture()
-	
-	term.sendEvent {type = "get_resolution", done = completer}
-	
-	local size = await(future)
+	local size = await(term.sendEvent {type = "get_resolution"})
 	return size.width, size.height
 end
 
@@ -112,13 +113,13 @@ end
 function term.setCursor(x, y)
 	checkArg(1, x, "number")
 	checkArg(2, y, "number")
-	
-	term.sendEvent {type = "move_cursor", x = x, y = y}
+
+	return term.sendEvent {type = "move_cursor", x = x, y = y}
 end
 
 function term.getCursor()
 	local state = await(term.getState())
-	
+
 	return state.cursorX, state.cursorY
 end
 
@@ -126,13 +127,13 @@ end
 
 function term.setCursorBlink(blink)
 	blink = not not blink
-	
-	term.sendEvent {type = "set_blink", blink = blink}
+
+	return term.sendEvent {type = "set_blink", blink = blink}
 end
 
 function term.getCursorBlink()
 	local state = await(term.getState())
-	
+
 	return state.cursorBlink
 end
 
@@ -140,14 +141,14 @@ end
 
 function term.write(str)
 	str = tostring(str)
-	
-	term.sendEvent {type = "write", text = str}
+
+	return term.sendEvent {type = "write", text = str}
 end
 
 function term.put(x, y, str)
 	str = tostring(str)
-	
-	term.sendEvent {type = "put", x = x, y = y, text = str}
+
+	return term.sendEvent {type = "put", x = x, y = y, text = str}
 end
 
 function term.copy(x, y, width, height, destx, desty)
@@ -157,8 +158,13 @@ function term.copy(x, y, width, height, destx, desty)
 	checkArg(4, height, "number")
 	checkArg(5, destx, "number")
 	checkArg(6, desty, "number")
-	
-	term.sendEvent {type = "copy", x = x, y = y, width = width, height = height, destx = destx, desty = desty}
+
+	return term.sendEvent {type = "copy", x = x, y = y, width = width, height = height, destx = destx, desty = desty}
+end
+
+function term.scrollVertical(amount)
+	checkArg(1, amount, "number", "nil")
+	return term.sendEvent {type = "scroll_vertical", amount = amount}
 end
 
 --
@@ -167,7 +173,7 @@ function term.read(line, history, tabResolver, pwchar, signalStream)
 	if not ready then
 		term.init()
 	end
-	
+
 	checkArg(1, line, "string", "nil")
 	checkArg(2, history, "table", "nil")
 	--TODO: Full tabResolver and pwchar support
@@ -175,28 +181,28 @@ function term.read(line, history, tabResolver, pwchar, signalStream)
 	checkArg(4, pwchar, "string", "nil")
 	signalStream = signalStream or await(getSignalStream())
 	kobject.checkType(signalStream, "ReadStream")
-	
+
 	line = line or ""
 	local cursor = #line
 	local baseCursorX, baseCursorY = term.getCursor(pid, handle)
 	local width, height = term.getSize(pid, handle)
 	local lineWidth = width-baseCursorX+1
 	local scroll = math.max(#line-lineWidth+1, 0)
-	
+
 	local function redraw()
 		term.put(baseCursorX, baseCursorY, (" "):rep(lineWidth)) --writes, but does not do wrapping or cursor setting
-		
+
 		term.put(baseCursorX, baseCursorY, line:sub(scroll+1, scroll+lineWidth+1))
-		
+
 		term.setCursor(baseCursorX+(cursor-scroll), baseCursorY)
 	end
-	
+
 	local function checkCursor()
 		local screenSpaceCursor = (cursor-scroll+1)-lineWidth
-		
+
 		if screenSpaceCursor > 0 then
 			scroll = scroll+screenSpaceCursor
-			
+
 			--[[term.copy(pid, handle,
 				baseCursorX+screenSpaceCursor, baseCursorY,
 				lineWidth-screenSpaceCursor, 1,
@@ -204,7 +210,7 @@ function term.read(line, history, tabResolver, pwchar, signalStream)
 			term.put(pid, handle,
 				baseCursorX+(lineWidth-screenSpaceCursor-1), baseCursorY,
 				line:sub(scroll+(lineWidth-screenSpaceCursor), scroll+lineWidth))
-			
+
 			if #line-scroll < lineWidth then
 				local c = lineWidth-(#line-scroll)
 				term.put(pid, handle,
@@ -214,7 +220,7 @@ function term.read(line, history, tabResolver, pwchar, signalStream)
 		elseif screenSpaceCursor <= (-lineWidth)+1 then
 			screenSpaceCursor = screenSpaceCursor+lineWidth
 			scroll = math.max(scroll-(lineWidth+1), 0) --lineWidth+1
-			
+
 			--[[term.copy(pid, handle,
 				baseCursorX, baseCursorY,
 				lineWidth-screenSpaceCursor, 1,
@@ -222,7 +228,7 @@ function term.read(line, history, tabResolver, pwchar, signalStream)
 			term.put(pid, handle,
 				baseCursorX, baseCursorY,
 				line:sub(scroll+1, scroll+(lineWidth-screenSpaceCursor)+1))
-			
+
 			if #line-scroll < lineWidth then
 				local c = lineWidth-(#line-scroll)
 				term.put(pid, handle,
@@ -230,30 +236,30 @@ function term.read(line, history, tabResolver, pwchar, signalStream)
 					(" "):rep(c))
 			end]]
 		end
-		
+
 		term.setCursor(baseCursorX+(cursor-scroll), baseCursorY)
-		
+
 		return screenSpaceCursor > 0
 	end
-	
+
 	local function insert(str)
 		if cursor == #line then
 			line = line..str
 		elseif cursor == 0 then
 			line = str..line
 		else
-			line = line:sub(1, cursor-1)..str..line:sub(cursor+1)
+			line = line:sub(1, cursor)..str..line:sub(cursor+1)
 		end
-		
+
 		cursor = cursor+#str
-		
+
 		if not checkCursor() then
 			--term.put(pid, handle, baseCursorX+(cursor-scroll-1), baseCursorY, str)
 		end
-		
+
 		redraw()
 	end
-	
+
 	local function backspace()
 		if cursor == #line+1 then
 			line = line:sub(1, -2)
@@ -262,16 +268,16 @@ function term.read(line, history, tabResolver, pwchar, signalStream)
 		else
 			return
 		end
-		
+
 		cursor = cursor-1
-		
+
 		if not checkCursor() then
 			--[[local copyWidth = math.min(#line-cursor, lineWidth-(cursor-scroll))
 			term.copy(pid, handle,
 				baseCursorX+(cursor-scroll)+1, baseCursorY,
 				copyWidth, 1,
 				baseCursorX+(cursor-scroll), baseCursorY)
-			
+
 			local rep = line:sub(cursor+copyWidth, cursor+copyWidth)
 			if #rep == 0 then rep = " " end
 			os.logf("TERM", "copychar %s", rep)
@@ -283,18 +289,18 @@ function term.read(line, history, tabResolver, pwchar, signalStream)
 			--term.put(pid, handle, baseCursorX+(cursor-scroll), baseCursorY, " ")
 			--redraw()
 		end
-		
+
 		os.logf("TERM", "%d %d %s", cursor, scroll, line)
 		redraw()
 	end
-	
+
 	redraw()
-	
+
 	local completer, future = kobject.newFuture()
-	
+
 	signalStream:listen(function(signal)
 		local typ = table.remove(signal, 1)
-		
+
 		if typ == "key_down" then
 			local char = signal[2]
 			if not (char < 0x20 or (char >= 0x7F and char <= 0x9F)) then
@@ -309,15 +315,24 @@ function term.read(line, history, tabResolver, pwchar, signalStream)
 				cursor = cursor+1
 				checkCursor()
 				redraw()
+			elseif signal[3] == 0xC7 then -- home
+				cursor = 0
+				checkCursor()
+				redraw()
+			elseif signal[3] == 0xCF then -- end
+				cursor = #line
+				checkCursor()
+				redraw()
 			elseif signal[3] == 0x1C then
+        os.logf("TERM", "Complete line %s", line)
 				completer:complete(line)
 				signalStream:close()
 			end
 		end
 	end)
-	
+
 	return future
-	
+
 	--return await(srv:invoke("read", handle, await(getSignalStream()), history))
 end
 
